1ad41620f8ea62112ad3619e245d21f3bb0579a2
Gwaeb <gwaeb@hotmail.com>
2021-11-08 21:58:51 -0500

    OpenWeatherService complete - 1935990

---
+++ WeatherApp/Services/OpenWeatherService.cs
@@ -0,0 +1,38 @@
+﻿using OpenWeatherAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Models;
+
+namespace WeatherApp.Services
+{
+    public class OpenWeatherService : IWindDataService
+    {
+        private static OpenWeatherProcessor owp;
+
+        public WindDataModel LastWindData;
+
+        public OpenWeatherService(string apiKey)
+        {
+            owp = OpenWeatherProcessor.Instance;
+            owp.ApiKey = apiKey;
+        }
+
+        public async Task<WindDataModel> GetDataAsync()
+        {
+            OWCurrentWeaterModel result = await owp.GetCurrentWeatherAsync();
+
+            WindDataModel wdm = new WindDataModel();
+
+            wdm.DateTime = DateTime.FromBinary(result.DateTime);
+            wdm.MetrePerSec = result.Wind.Speed;
+            wdm.Direction = result.Wind.Deg;
+
+            LastWindData = wdm;
+
+            return LastWindData;
+        }        
+    }
+}
